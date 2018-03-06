89eec2348e9a20a8254fa5dd50f14c656530ca1c
Nagabhushanam <nagabhushanamn@gmail.com>
2018-03-07 02:36:07 +0530

    new commit on change1

--- web-calculator/acceptance_test.sh
+++ web-calculator/acceptance_test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-test $(curl localhost:8765/sum?a=10\&b=20) -eq 30
+#test $(curl localhost:8765/sum?a=10\&b=20) -eq 30
+
+
+#!/bin/bash
+CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
+test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
